97804e4470fc31a1eaf453701eeddaf643718926
HenzoWTF <Urenahenzo@gmail.com>
2024-04-10 19:31:34 -0400

    Colocacion de llavez

--- Library/Barbero.cs
+++ Library/Barbero.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ using System.Threading.Tasks;
 namespace Library;
 public class Barbero
 {
+    [Key]
     public int BarberoId { get; set; }
     public string BarberoName { get; set; }
     public string Celular { get; set; }
     public string Direccion { get; set; }
+    public float Sueldo {  get; set; }
+    public float Comision { get; set; }
 
     [ForeignKey("BarberoId")]
     public List<Factura> Facturas { get; set; } = new List<Factura>();
